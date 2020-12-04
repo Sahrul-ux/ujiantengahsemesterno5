71d0353b140bad31cd64e5f261b1db871218bbfb
Sahrul Rojab <sahrulrajab11@gmail.com>
2020-12-04 10:58:15 +0700

    ujian tengah semester no. 5

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	int result;
+	
+	a = 8 ;
+	b = 2 ;
+	a = a + 1;
+	result = a - b;
+	cout << "Hasil (int) = " << result;
+	getch();
+	
+	return 0;
+}
